84aeba5812a48af2937988e9a365683e45475255
Kevin Huang <huangyf9883@gmail.com>
2026-05-15 21:13:17 +0800

    fix: Eyewear device type

--- gen_csv.py
+++ gen_csv.py
@@ -52,7 +52,7 @@ _re_non_word = re.compile(r'[\W_]+')
 _re_model_ver = re.compile(r'^`(([^`]+)`\s*)+:\s*')
 _re_model_item = re.compile(r'`([^`]+)`')
 # 匹配设备类型的正则
-_re_device_type = re.compile(r'(手机|手表|手环|平板|电视主机|盒子|(智能)?电视|笔记本电脑|设备|穿戴|眼镜|Mobile|Phone|Pad|Pod|Tablet|Watch|Band|WATCH|Device|Glass|\bTV\b|学习智慧屏|智慧屏)')
+_re_device_type = re.compile(r'(手机|手表|手环|平板|电视主机|盒子|(智能)?电视|笔记本电脑|设备|穿戴|眼镜|Mobile|Phone|Pad|Pod|Tablet|Watch|Band|WATCH|Device|Glass|Eyewear|\bTV\b|学习智慧屏|智慧屏)')
 _device_map = dict(
     手机='mob',
     mobile='mob',
@@ -71,7 +71,8 @@ _device_map = dict(
     电视主机='tv_hub',
     盒子='tv_hub',
     眼镜='glass',
-    Glass='glass'
+    Glass='glass',
+    Eyewear='glass'
 )
 
 pd_cols = 'model,dtype,brand,brand_title,code,code_alias,model_name,ver_name'.split(',')
